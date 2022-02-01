3125e5957dc765c4177c14450fb681dfd1bf91bb
lpleme <lem17001@byui.edu>
2022-01-31 23:04:39 -0700

    Added name.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Benjamin Palmer.");
+        System.out.println("Hello Benjamin Palmer, my name is Lorenzo.");
     }
 }
